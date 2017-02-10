dd38802e561d936877bba6b19e4727beb9cf9ab7
adamingwersen <adamlinnemann@gmail.com>
2017-02-10 06:08:31 +0100

    init

---
+++ Exercises/Geometry/Geometry/Vertex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Geometry
+{
+    public struct Vertex
+    {
+        public int x;
+        public int y;
+
+        public override string ToString()
+        {
+            return "(" + x.ToString() + ", " + y.ToString() + ")";
+        }
+    }
+}
+
+
